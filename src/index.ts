257608cd112d3b1f81917f0440e9653b36a3beb8
martas <mart.susek@gmail.com>
2019-06-22 10:40:00 +0200

    Sparate YtConverter class into a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-export default class YtConverter {
-
-    /**
-     * @param  {Array<string>} urls
-     * - array of video urls to download
-     * @param  {string} dir - optional
-     * - destination directory - default /home/{user}/videos
-     * @param  {boolean} convert - optional
-     *  - should the videos be converted - default: false
-     * @param  {string} format - optional
-     *  - format to convert into - default: "mp3"
-     */
-    async init(urls?: Array<string>, dir?: string, convert?: boolean, format?: string, update?: boolean) {
-    }
-}
+import YtConverter from "./YtConverter"
 
 export function init(urls?: Array<string>, dir?: string, convert?: boolean, format?: string) {
-    new YtConverter().init(urls, dir, convert, format).catch((err:Error) => {
+    new YtConverter().init(urls, dir, convert, format).catch((err: Error) => {
         console.error("ERROR", err)
         process.exit(1)
     })
